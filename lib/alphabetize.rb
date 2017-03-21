2b4c649be208bc7f6da12c73af32ffa984e20674
szrharrison <s.z.r.harrison@gmail.com>
2017-03-21 17:27:17 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+require 'pry'
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  sorted_array = arr.sort_by do |string|
+    string.chars.map do |char|
+      ESPERANTO_ALPHABET.index(char)
+    end
+  end
 end
